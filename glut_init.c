94dd2fc030759a8a9e830589ff5f102c63a77f5d
lp723 <aphiloc@gmail.com>
2023-07-04 19:29:18 -0700

    Barebones open window

--- glut_init.c
+++ glut_init.c
@@ -1,7 +1,11 @@
-#include <GL/glut.h>
-#include <GL/freeglut_ext.h>
+
+#include <GL/freeglut.h>
 
 unsigned int defaults(unsigned int displayMode, int *width, int *height);
+void init();
+void display();
+void reshape(int w, int h);
+void keyboard(unsigned char key, int x, int y);
 
 int main(int argc, char** argv)
 {
@@ -14,8 +18,35 @@ int main(int argc, char** argv)
 	glutInitDisplayMode (displayMode);
 	glutInitContextVersion (3, 3);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
+#ifdef DEBUG
+	glutInitContextFlags(GLUT_DEBUG);
+#endif
+	glutInitWindowSize (width, height); 
+	glutInitWindowPosition (300, 200);
+	int window = glutCreateWindow (argv[0]);
+
+	//glload::LoadFunctions();
+	
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
+	
+	init();
+
+	glutDisplayFunc(display); 
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
 }
 
+//Called after the window and OpenGL are initialized. Called exactly once, before the main loop.
+void init()
+{
+	//InitializeProgram();
+	//InitializeVertexBuffer();
+
+	//glGenVertexArrays(1, &vao);
+	//glBindVertexArray(vao);
+}
 
 // this should be moved out of the framework
 unsigned int defaults(unsigned int displayMode, int *width, int *height) {
@@ -23,3 +54,35 @@ unsigned int defaults(unsigned int displayMode, int *width, int *height) {
     
     return displayMode;
 }
+
+//Called to update the display.
+//You should call glutSwapBuffers after all of your rendering to display what you rendered.
+//If you need continuous updates of the screen, call glutPostRedisplay() at the end of the function.
+void display()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+}
+
+//Called whenever the window is resized. The new window size is given, in pixels.
+//This is an opportunity to call glViewport or glScissor to keep up with the change in size.
+void reshape (int w, int h)
+{
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+}
+
+//Called whenever a key on the keyboard was pressed.
+//The key is given by the ''key'' parameter, which is in ASCII.
+//It's often a good idea to have the escape key (ASCII value 27) call glutLeaveMainLoop() to 
+//exit the program.
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	  case 27:
+		  glutLeaveMainLoop();
+		  return;
+	}
+}
+
